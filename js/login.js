87a007786ceae39deaac5c4072e4b98d94ca8916
Tamema-TT <tamema.tama@northsouth.edu>
2022-10-17 20:59:24 +0600

    Complete Work

--- js/login.js
+++ js/login.js
@@ -1,8 +1,8 @@
 //login
-document.getElementById('login-submit').addEventListener('click', function() {
-    const emailField = document.getElementById('user-email');
-    const passField = document.getElementById('user-pass');
-    if(emailField.value == 'hi@gmail.com' && passField.value == 'hi') {
-        window.location.href = 'banking.html';
-    }
-});
+// document.getElementById('login-submit').addEventListener('click', function() {
+//     const emailField = document.getElementById('user-email');
+//     const passField = document.getElementById('user-pass');
+//     if(emailField.value == 'hi@gmail.com' && passField.value == 'hi') {
+//         window.location.href = 'banking.html';
+//     }
+// });
